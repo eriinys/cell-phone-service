af7b6a4f918632dc4a42e7d69da0fc78444dc2e2
eriinys <eriinys@duck.com>
2025-09-30 18:47:21 -0500

    added getter/setter on CellPhone.java

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+public class CellPhone {
+    //private
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private  String owner;
+
+    //parameterless constructor
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    public CellPhone(int serialNumber, String phoneNumber) {
+        setSerialNumber(serialNumber);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getPhoneNumber(String phoneNumber) {
+        return  phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+}
+
+
